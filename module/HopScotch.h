7053442fe1b528286b93ad2da5333f09c21e86aa
YoshinoTakumi <i50347@ga.ariake-nct.ac.jp>
2020-09-11 12:31:40 +0900

    へっだにちょっと肉付けした

--- module/HopScotch.h
+++ module/HopScotch.h
@@ -1,4 +1,19 @@
 #ifndef HOPSCOTCH_H
 #define HOPSCOTCH_H
 
-#endif//HOPSCOTCH_H
+#include "Controller.h"
+#include "MoveStraight.h"
+#include "LineTracer.h"
+#include "Rotation.h"
+
+enum Direction { North, NEast, East, SEast, South, SWest, West, NWest };
+
+class HopScotch {
+ public:
+  hopscotch(Controller& ctrler_, MoveStraight& linear_, LineTracer& tracer_, Rotation& yaw_);
+  void cnvrtC8E2Motion(std::vector<std::vector<int>>& c8e_, Direction direct_);
+
+ private:
+};
+
+#endif  // HOPSCOTCH_H
