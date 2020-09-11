3c88e5a7bdd6d571de43d4eed38c011a13b526bb
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-09-12 06:09:13 +0900

    とりあえず書いた。中身は保証できない

--- module/HopScotch.cpp
+++ module/HopScotch.cpp
@@ -1,11 +1,49 @@
 #include "HopScotch.h"
+#include <vector>
 
 HopScotch::HopScotch(Controller& ctrler_, MoveStraight& linear_, LineTracer& tracer_, Rotation& yaw_)
+: ctrler(ctrler_), linear(linear_), tracer(tracer_), yaw(yaw_)
 {
-
+  
 }
 
 void HopScotch::cnvrtC8E2Motion(std::vector<std::vector<int>>& c8es_, Direction direct_)
 {
-    
+ 
+}
+
+void HopScotch::vectordiff(vector<vector<int>> & c8es_)
+{
+    // c8es_[n][n] = {{x0, y0}, {x1, y1}, ... , {xn, yn}}    
+    for(int i = 0; i <= c8es_.at(0).size(); i++) {  // 横の配列読み取り
+
+
+        for(int j = 0; j <= c8es_.size(); j++) {      // 縦の配列読み取り
+
+            HopScotch::y_diff.push_back(c8es_[i+1][j] - c8es_[i][j]);
+            HopScotch::x_diff.push_back(c8es_[j][i+1] - c8es_[j][i]);
+        }
+    }
+}
+
+// Rotatesion.cpp rotate関数の引数にprivateの角度を渡す
+
+// LineTracer:: runとruntocolor
+
+
+// MoveStraightの moveto　引数に距離または角度
+// 後退もできる
+
+
+void HopScotch::state()
+{
+  static void (HopScotch::*stateMachine[])() = {
+    &HopScotch::cb_crossToMidPoint,
+    cb_midPointToCross,
+    cb_yawing,
+    cb_linear,
+    cb_midToMid
+  };
+
+
 }
--- module/HopScotch.h
+++ module/HopScotch.h
@@ -7,14 +7,17 @@
 #ifndef HOPSCOTCH_H
 #define HOPSCOTCH_H
 
+#include <vector>
 #include "Controller.h"
 #include "MoveStraight.h"
 #include "LineTracer.h"
 #include "Rotation.h"
-#include <vector>
 
-//※このフォーマットで適切か菅くんに要確認　9/11/2020
-enum Direction { North, NEast, East, SEast, South, SWest, West, NWest };
+using namespace std;
+
+//※このフォーマットで適切か菅くんに要確認　9/11/2020　ヨシ
+enum class Direction { North, NEast, East, SEast, South, SWest, West, NWest };
+
 
 class HopScotch {
  public:
@@ -28,11 +31,33 @@ class HopScotch {
 
   /** 座標->機動変換メソッド
    * @param c8es_ 整数型座標ベクタ (c8es = coordinates)
-   * @param direct_ 列挙型向き情報
+   * @param direct_ 向き情報
    **/
-  void cnvrtC8E2Motion(std::vector<std::vector<int>>& c8es_, Direction direct_);
+  void cnvrtC8E2Motion(vector<vector<int>>& c8es_, Direction direct_);
+
 
  private:
+    Controller&     ctrler; //参照型コントローラクラス
+    MoveStraight&   linear; //参照型直線運動クラス
+    LineTracer&     tracer; //参照型ライントレーサクラス
+    Rotation&       yaw;    //参照型回頭クラス
+
+    void cb_crossToMidPoint(); //交点->中点
+    void cb_midPointToCross(); //中点->交点
+    void cb_yawing();          //方向転換
+    void cb_linear();          //交点∪中点 -> ブロックサークル
+    void cb_midToMid();        //中点->中点
+    void state();
+
+    //二点間のx差分
+    // 座標どうしの差分
+    vector<int> x_diff; //二点間のx差分
+    vector<int> y_diff; //二点間のy差分    
+    void vectordiff(vector<vector<int>> & c8es_);
+
+
+    // 座標どうしの差分から方角をとる
+    
 };
 
-#endif  // HOPSCOTCH_H
+#endif//HOPSCOTCH_H
