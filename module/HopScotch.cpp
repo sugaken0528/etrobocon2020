a1ee17522e60d45a7b01500ef915d9768457b87e
YoshinoTakumi <i50347@ga.ariake-nct.ac.jp>
2020-09-12 06:18:34 +0900

    ウルトラ眠いなりにクラス記述

--- module/HopScotch.cpp
+++ module/HopScotch.cpp
@@ -1,10 +1,9 @@
 #include "HopScotch.h"
 
-HopScotch::HopScotch(Controller& ctrler_, MoveStraight& linear_, LineTracer& tracer_, Rotation& yaw_)
-    : ctrler(ctrler_), linear(linear_), tracer(tracer_), yaw(yaw_)
-{}
-
-void HopScotch::cnvrtC8E2Motion(std::vector<std::vector<int>>& c8es_, Direction direct_)
+HopScotch::HopScotch(Controller& ctrler_, MoveStraight& linear_, LineTracer& tracer_,
+                     Rotation& yaw_)
+  : ctrler(ctrler_), linear(linear_), tracer(tracer_), yaw(yaw_)
 {
-
 }
+
+void HopScotch::cnvrtC8E2Motion(std::vector<std::vector<int>>& c8es_, Direction direct_) {}
