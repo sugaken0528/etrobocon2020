8f9a27e2e55f984389b5393a26e1c2e546272f67
YoshinoTakumi <i50347@ga.ariake-nct.ac.jp>
2020-09-11 12:52:39 +0900

    へっだにちょっと肉付けした, Makefileにすこっちオブジェクト追加

--- module/HopScotch.h
+++ module/HopScotch.h
@@ -1,3 +1,9 @@
+/**
+ * @file HopScotch.h
+ * @brief 座標情報を機動に変換するクラス
+ * @author T.Yoshino(Mikoyaan), D.Hirayama(Kanikama)
+ **/
+
 #ifndef HOPSCOTCH_H
 #define HOPSCOTCH_H
 
@@ -6,12 +12,24 @@
 #include "LineTracer.h"
 #include "Rotation.h"
 
+//※このフォーマットで適切か菅くんに要確認　9/11/2020
 enum Direction { North, NEast, East, SEast, South, SWest, West, NWest };
 
 class HopScotch {
  public:
+  /** コンストラクタ
+   * @param　ctrler_ 参照型コントローラクラス
+   * @param  linear_ 参照型直線運動クラス
+   * @param  tracer_ 参照型ライントレーサクラス
+   * @param  yaw_    参照型回頭クラス
+   **/
   hopscotch(Controller& ctrler_, MoveStraight& linear_, LineTracer& tracer_, Rotation& yaw_);
-  void cnvrtC8E2Motion(std::vector<std::vector<int>>& c8e_, Direction direct_);
+
+  /** 座標->機動変換メソッド
+   * @param c8es_ 整数型座標ベクタ (c8es = coordinates)
+   * @param direct_ 列挙型向き情報
+   **/
+  void cnvrtC8E2Motion(std::vector<std::vector<int>>& c8es_, Direction direct_);
 
  private:
 };
